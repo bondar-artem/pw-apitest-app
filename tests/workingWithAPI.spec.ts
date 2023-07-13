af7112d5196e197de466f166eb3e0221fd636acc
Artem Bondar <artem.bondar@gmail.com>
2023-07-13 17:18:01 -0500

    tags call

--- tests/workingWithAPI.spec.ts
+++ tests/workingWithAPI.spec.ts
@@ -1,6 +1,13 @@
 import { test, expect } from '@playwright/test';
+import tags from '../test-data/tags.json'
 
 test.beforeEach( async({page}) => {
+  await page.route('*/**/api/tags', async route => {
+    await route.fulfill({
+      body: JSON.stringify(tags)
+    })
+  })
+
   await page.goto('https://angular.realworld.io/');
 })
 
