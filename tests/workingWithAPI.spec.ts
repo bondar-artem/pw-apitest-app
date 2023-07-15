32ae9a78171e5ca2f77cf6f9c238f42f8a49e74e
Artem Bondar <artem.bondar@gmail.com>
2023-07-15 10:12:17 -0500

    modify response

--- tests/workingWithAPI.spec.ts
+++ tests/workingWithAPI.spec.ts
@@ -8,11 +8,24 @@ test.beforeEach( async({page}) => {
     })
   })
 
+  await page.route('*/**/api/articles*', async route => {
+    const response = await route.fetch()
+    const responseBody = await response.json()
+    responseBody.articles[0].title = "This is a test title"
+    responseBody.articles[0].description = "This is a desctiption"
+
+    await route.fulfill({
+      body: JSON.stringify(responseBody)
+    })
+  })
+
   await page.goto('https://angular.realworld.io/');
 })
 
 test('has title', async ({ page }) => {
   await expect(page.locator('.navbar-brand')).toHaveText('conduit');
+  await expect(page.locator('app-article-list h1').first()).toContainText('This is a test title')
+  await expect(page.locator('app-article-list p').first()).toContainText('This is a desctiption')
 });
 
 
